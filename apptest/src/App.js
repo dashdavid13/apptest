847e021c68963ff7df9f950377882dc8dff53084
David <davidamedina619@gmail.com>
2021-09-19 10:20:02 -0400

    adding components with flow

--- apptest/src/App.js
+++ apptest/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./Home"
+import Login from "./Login"
+import MainCard from "./MainCard"
+import { Switch, Route, useHistory} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Home />
+     <Login />
+     <MainCard />
     </div>
   );
 }
